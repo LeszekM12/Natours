46dff6e388b10ef5d8920b6bdee62c40da92819c
Leszek Mikrut <leszekmikrut60@gmail.com>
2025-11-09 21:14:43 +0100

    Implements tours route handler for post

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -23,6 +25,24 @@ app.get('/api/v1/tours', (req, res) => {
     data: {
       tours
     }
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, res.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
   });
 });
 
