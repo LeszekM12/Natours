5eeef9a0063ef720dd18b83fee951f2286bf131e
Leszek Mikrut <leszekmikrut60@gmail.com>
2025-11-22 21:14:00 +0100

    Another Way of Creating Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,25 +1,15 @@
-// const Tour = require('./../models/tourModel');
+const Tour = require('./../models/tourModel');
 
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf8')
 // );
 
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Bad Request for name or price'
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
-    requestedAt: req.requestTime,
+    requestedAt: req.requestTime
     // results: tours.length,
     // data: {
     //   tours
@@ -31,23 +21,35 @@ exports.getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
 
-    // const tour = tours.find(tour => tour.id === id);
-    //
-    // res.status(200).json({
-    //   status: 'success',
-    //   data: {
-    //     tour
-    //   }
-    // });
+  // const tour = tours.find(tour => tour.id === id);
+  //
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour
+  //   }
+  // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour
-    // }
-  });
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({})
+    // newTour.save()
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
+
 };
 
 exports.updateTour = (req, res) => {
