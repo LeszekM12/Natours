ad01ecd95107b3989f9d63792cd5194cb203d48c
Leszek Mikrut <leszekmikrut60@gmail.com>
2025-12-01 15:30:15 +0100

    Solving business problem calculating busiest month of a given year using unwind, projecting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -30,14 +30,14 @@ exports.getAllTours = async (req, res) => {
     res.status(404).json({
       status: 'failed',
       message: err.message
-    })
+    });
   }
 };
 
 exports.getTour = async (req, res) => {
   try {
- const tour = await Tour.findById(req.params.id);
- // Tour.findOne({ _id: req.params.id })
+    const tour = await Tour.findById(req.params.id);
+    // Tour.findOne({ _id: req.params.id })
     res.status(200).json({
       status: 'success',
       data: {
@@ -48,7 +48,7 @@ exports.getTour = async (req, res) => {
     res.status(404).json({
       status: 'failed',
       message: err.message
-    })
+    });
   }
 };
 
@@ -78,7 +78,7 @@ exports.updateTour = async (req, res) => {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true
-    })
+    });
 
     res.status(200).json({
       status: 'success',
@@ -90,7 +90,7 @@ exports.updateTour = async (req, res) => {
     res.status(404).json({
       status: 'fail',
       message: err.message
-    })
+    });
   }
 
 };
@@ -107,7 +107,7 @@ exports.deleteTour = async (req, res) => {
     res.status(404).json({
       status: 'failed',
       message: err.message
-    })
+    });
   }
 
 };
@@ -116,26 +116,23 @@ exports.getTourStats = async (req, res) => {
   try {
     const stats = await Tour.aggregate([
       {
-        $match: { ratingsAverage: { $gte: 4.5 }}
+        $match: { ratingsAverage: { $gte: 4.5 } }
       },
       {
         $group: {
-          _id :{ $toUpper: '$difficulty' },
-          numTours : { $sum: 1 },
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
           numRatings: { $sum: '$ratingsQuantity' },
           avgRating: { $avg: '$ratingsAverage' },
           avgPrice: { $avg: '$price' },
           minPrice: { $min: '$price' },
-          maxPrice: { $max: '$price' },
+          maxPrice: { $max: '$price' }
         }
       },
       {
         $sort: {
           avgPrice: 1
         }
-      },
-      {
-
       }
     ]);
 
@@ -148,7 +145,58 @@ exports.getTourStats = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'failed',
-      message : err.message
-    })
+      message: err.message
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates'
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`)
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' }
+        }
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: { _id: 0  }
+      },
+      {
+        $sort: { numTourStarts: -1 }
+      },
+      {
+        $limit: 12
+      }
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failed',
+      message: err.message
+    });
   }
 };
