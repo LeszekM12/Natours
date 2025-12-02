0ca6b68a57e80eaf208cc196b0879933488b49bd
Leszek Mikrut <leszekmikrut60@gmail.com>
2025-12-02 14:38:00 +0100

    Aggregation Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -93,6 +93,13 @@ tourSchema.post(/^find/, function(docs, next) {
   next();
 });
 
+// Aggregation Middleware
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline.unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
