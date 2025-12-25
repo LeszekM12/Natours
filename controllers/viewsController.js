b282e4985f02270912031cf31d9a03669a9bfacf
Leszek Mikrut <leszekmikrut60@gmail.com>
2025-12-25 23:45:04 +0100

    Building the Tour Overview Page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res) => {
+const Tour =  require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  //  Get all tour data from collection
+  const tours = await Tour.find();
+  // Build template
+
+  // Render that template using tour data from first step
   res.status(200).render('overview', {
-    title: 'All tours'
+    title: 'All tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
