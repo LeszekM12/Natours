90d9940baa633888bc9e4f9f08171622d09178f2
Leszek Mikrut <leszekmikrut60@gmail.com>
2025-12-28 19:30:07 +0100

    Logging in Users with API - step 2.

--- controllers/authController.js
+++ controllers/authController.js
@@ -89,6 +89,28 @@ exports.protect = catchAsync(async (req, res, next) => {
   next();
 });
 
+// Only for rendered pages, no errors!
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  if (req.cookies.jwt) {
+    // verify token
+    token = req.cookies.jwt;
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    // Check if user still exists/not expired
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) return next();
+
+    // Check if user changed password after token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next();
+    }
+    // There is a logged in user
+    res.locals.user = currentUser;
+    return next();
+  }
+  next();
+});
+
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
