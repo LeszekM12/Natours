7527a295367b30435d16bfbec2a4565fa76b86a3
Leszek Mikrut <leszekmikrut60@gmail.com>
2026-01-10 22:23:01 +0100

    Cutting host links

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -4,7 +4,7 @@ const stripe = Stripe('pk_test_51SmzubA6Mjyk41hlO8KGDRlLCfvQTBwUMVUpc9rXw2RFmiBp
 const bookTour = async tourId => {
   try {
     // Get checkout session from API
-    const session = await axios(`http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     // Create checkout form + charge credit card
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id
