622d7638a718ad34382249e8a946eac3eba9cd57
Leszek Mikrut <leszekmikrut60@gmail.com>
2026-01-13 17:41:03 +0100

    Missing "?" for req.secure

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,7 @@ const createSendToken = catchAsync(async (user, statusCode, req, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    secure: req?.secure || req?.headers['x-forwarded-proto'] === 'https',
     sameSite: 'None'
   }
 
