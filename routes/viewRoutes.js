1e13c9c5f4c36f7ae51f7a04f470ece7d0bd88c2
Leszek Mikrut <leszekmikrut60@gmail.com>
2026-01-18 17:33:42 +0100

    Implemented My Reviews Page for all users reviews

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -14,6 +14,7 @@ router.get('/forgot-password', viewsController.getForgotPasswordForm);
 router.get('/reset-password/:token', viewsController.getResetPasswordForm);
 router.get('/me', authController.protect, viewsController.getAccount);
 router.get('/my-tours', authController.protect, viewsController.getMyTours);
+router.get('/my-reviews', authController.protect, viewsController.getMyReviews);
 
 router.post('/forgot-password', authController.forgotPassword);
 
